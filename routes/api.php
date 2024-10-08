de4add9408267ed8f5b958ce5df32cb2c7ed97e9
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-08 15:39:02 +0800

    Setting Up Authentication Using Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,16 @@
 <?php
 
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::apiResource('events', EventController::class);
 
