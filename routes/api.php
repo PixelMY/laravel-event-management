924616209a4b5fa4ac15410c0201d6563dd6160a
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-08 13:02:07 +0800

    Attendees and Pagination

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,4 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('events', EventController::class);
 
 Route::apiResource('events.attendees', AttendeeController::class)
-    ->scoped(['attendee' => 'event']);
+    ->scoped()->except(['update']);
